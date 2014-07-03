f1615b4a66adfd92d4ac7f4c15c3eb520cf8341f
Robin Burchell <robin+git@viroteck.net>
2014-07-03 21:06:32 +0000

    MER: More systrace logging

--- src/corelib/kernel/qeventdispatcher_glib.cpp
+++ src/corelib/kernel/qeventdispatcher_glib.cpp
@@ -43,6 +43,7 @@
 #include "qeventdispatcher_unix_p.h"
 
 #include <private/qthread_p.h>
+#include <private/qsystrace_p.h>
 
 #include "qcoreapplication.h"
 #include "qsocketnotifier.h"
@@ -99,6 +100,7 @@ static gboolean socketNotifierSourceCheck(GSource *source)
 
 static gboolean socketNotifierSourceDispatch(GSource *source, GSourceFunc, gpointer)
 {
+    QSystraceEvent systrace("io", "QEventDispatcher::socketNotifierSourceDispatch");
     QEvent event(QEvent::SockAct);
 
     GSocketNotifierSource *src = reinterpret_cast<GSocketNotifierSource *>(source);
@@ -178,6 +180,7 @@ static gboolean timerSourceCheck(GSource *source)
 
 static gboolean timerSourceDispatch(GSource *source, GSourceFunc, gpointer)
 {
+    QSystraceEvent systrace("io", "QEventDispatcher::timerSourceDispatch");
     GTimerSource *timerSource = reinterpret_cast<GTimerSource *>(source);
     if (timerSource->processEventsFlags & QEventLoop::X11ExcludeTimers)
         return true;
@@ -228,6 +231,7 @@ static gboolean idleTimerSourceCheck(GSource *source)
 
 static gboolean idleTimerSourceDispatch(GSource *source, GSourceFunc, gpointer)
 {
+    QSystraceEvent systrace("io", "QEventDispatcher::idleTimerSourceDispatch");
     GTimerSource *timerSource = reinterpret_cast<GIdleTimerSource *>(source)->timerSource;
     (void) timerSourceDispatch(&timerSource->source, 0, 0);
     return true;
@@ -274,6 +278,7 @@ static gboolean postEventSourceCheck(GSource *source)
 
 static gboolean postEventSourceDispatch(GSource *s, GSourceFunc, gpointer)
 {
+    QSystraceEvent systrace("io", "QEventDispatcher::postEventSourceDispatch");
     GPostEventSource *source = reinterpret_cast<GPostEventSource *>(s);
     source->lastSerialNumber = source->serialNumber.load();
     QCoreApplication::sendPostedEvents();
@@ -407,6 +412,7 @@ QEventDispatcherGlib::~QEventDispatcherGlib()
 bool QEventDispatcherGlib::processEvents(QEventLoop::ProcessEventsFlags flags)
 {
     Q_D(QEventDispatcherGlib);
+    QSystraceEvent systrace("io", "QEventDispatcher::processEvents");
 
     const bool canWait = (flags & QEventLoop::WaitForMoreEvents);
 
