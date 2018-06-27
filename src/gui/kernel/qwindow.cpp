sailfishos/qtbase

commit ad9eb8051f77ee94620bc12615e2dfb1e58699e3

[qtgui] Fix crash when a focused window's surface is destroyed. Contributes to JB#40880
Ensure full focus out and in are processed when transferring focus to the
parent window, otherwise the focusObjectChanged() signal of the window
whose window was destroyed will remain connected to
QGuiApplicationPrivate::_q_setFocusObject() and that can result in
an object not belonging to the active window (assuming there is one)
getting focus.

src/gui/kernel/qwindow.cpp
@@ -1651,7 +1651,7 @@ void QWindow::destroy()
     }
 
     if (QGuiApplicationPrivate::focus_window == this)
-        QGuiApplicationPrivate::focus_window = parent();
+        QWindowSystemInterface::handleWindowActivated(parent());
     if (QGuiApplicationPrivate::currentMouseWindow == this)
         QGuiApplicationPrivate::currentMouseWindow = parent();
     if (QGuiApplicationPrivate::currentMousePressWindow == this)

commit 9399340b4b351d06de95315bc453016bb0fa8106

[qtgui] fix crash when QWindow gets deleted. Fixes JB#42211
Earlier crash fix on commit ad9eb8051f77e caused another:
~QWindow() calls destroy() and if
QWindowSystemInterfacePrivate::handleWindowSystemEvent()
does asynchronous processing, QGuiApplicationPrivate::focus_window
will be already deleted at the time focus change gets handled.

Quite likely that focus change signaling isn't really proper with
this, but then again it's not crashing and hasn't been proper earlier
either.

src/gui/kernel/qwindow.cpp
@@ -203,6 +203,8 @@ QWindow::QWindow(QWindowPrivate &dd, QWindow *parent)
 QWindow::~QWindow()
 {
     destroy();
+    if (QGuiApplicationPrivate::focus_window == this)
+        QGuiApplicationPrivate::focus_window = 0;
     QGuiApplicationPrivate::window_list.removeAll(this);
     if (!QGuiApplicationPrivate::is_app_closing)
         QGuiApplicationPrivate::instance()->modalWindowList.removeOne(this);